7debd6147c15400dd4f04bba1fd71f255464d1ca
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-05-15 03:51:03 +0200

    Create admin_module.dart

---
+++ lib/admin/admin_module.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdminModule extends StatelessWidget {
+  const AdminModule({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
