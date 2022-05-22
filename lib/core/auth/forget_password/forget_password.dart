cfbc6c9de5b342d9e87862c74ee5aff43d07c955
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-05-22 08:26:07 +0200

    Create forget_password.dart

---
+++ lib/core/auth/forget_password/forget_password.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ForgetPassword extends StatelessWidget {
+  const ForgetPassword({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
