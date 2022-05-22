65c913a69265d7426bb6af9c00724679a33ee003
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-05-22 01:17:07 -0700

    create class color and font
    create class color and font

---
+++ lib/core/theme/colors/colors_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorsTheme{
+  static Color primary = const Color(0xff92FFA9);
+  static Color selectIcon = const Color(0xff035304);
+  static Color backgaroundDefault = const Color(0xffDADADA);
+  static Color backgaround = const Color(0xffDAD9CA);
+  static Color gray = const Color(0xffA1A1A1);
+
+}
---
+++ lib/core/theme/fonts_theme.dart
@@ -0,0 +1,6 @@
+class FontsTheme{
+  static const String fontFamily = "Montserrat";
+}
+class Font{
+  static const double s12 = 12.0 ;
+}
