7f110c162a5db6e1e45129862a2d119f0eafd3e6
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-05-24 08:38:10 +0200

    Update colors_theme.dart

--- lib/core/theme/colors/colors_theme.dart
+++ lib/core/theme/colors/colors_theme.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 
 class ColorsTheme{
   static Color wight = const Color(0xffFFFFFF);
+  static Color black = const Color(0xff000000);
   static Color primary = const Color(0xff92FFA9);
+  static Color iconUnselect = const Color(0xff77D18B);
   static Color green = const Color(0xff035304);
-  static Color backgaroundPage = const Color(0xffDADADA);
+  static Color dividerColor = const Color(0xff68C46B);
+  static Color backgaroundPage = const Color(0xffF5F4F4);
   static Color backgaround = const Color(0xffDAD9CA);
   static Color gray = const Color(0xffA1A1A1);
   static Color salseColor = const Color(0xffECEBEB);
