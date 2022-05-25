2dc6666ae6f501260af2b8d76c944b00b5848e96
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-05-24 20:55:31 -0700

    create account sales page

---
+++ lib/account_sales/account_sales_page/account_sales_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AccountSalesPage extends StatelessWidget {
+  const AccountSalesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
