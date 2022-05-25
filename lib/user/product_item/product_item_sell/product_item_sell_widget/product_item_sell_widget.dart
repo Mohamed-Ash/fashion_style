b99e33222e1b442439a14cf6da0d9010fbbf3c6e
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-05-24 20:50:54 -0700

    create product item page

---
+++ lib/user/product_item/product_item_sell/product_item_sell_widget/product_item_sell_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductItemSellWidget extends StatelessWidget {
+  const ProductItemSellWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
