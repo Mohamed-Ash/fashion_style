4a1008313cebec090a3b956bd77eeaffb72fdebb
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-05-24 20:50:11 -0700

    create setting page

---
+++ lib/user/settings/settings_page/settngs_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SettngsPage extends StatelessWidget {
+  const SettngsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/user/settings/settings_widget/settngs_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SettngsWigdet extends StatelessWidget {
+  const SettngsWigdet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
