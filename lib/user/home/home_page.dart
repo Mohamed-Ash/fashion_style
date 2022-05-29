b70bdc9644f5f8652452e14a52204243dbcb5e16
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-05-29 10:37:43 -0700

    add product item to home page

--- lib/user/home/home_page.dart
+++ lib/user/home/home_page.dart
@@ -1,34 +1,19 @@
 
 // ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, prefer_const_literals_to_create_immutables, must_be_immutable, void_checks, unused_field
 
-import 'package:fashion_style/core/theme/colors/colors_theme.dart';
 import 'package:fashion_style/user/carousel_slider/carousel_slider_page.dart';
 import 'package:fashion_style/user/carousel_slider/test_image_page.dart';
+import 'package:fashion_style/user/product_item/product_page/product_item_page.dart';
 import 'package:flutter/material.dart';
-class HomePage extends StatefulWidget {
-
-   const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  bool select = false ;
-  IconData iconselect = Icons.favorite;
-  IconData iconUnSelect = Icons.favorite_outline_sharp;
-
-  bool select2 = false ;
-  IconData iconselect2 = Icons.favorite;
-  IconData iconUnSelect2 = Icons.favorite_outline_sharp;
-
-  
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: SingleChildScrollView( 
+      child: SingleChildScrollView(
+        physics: ScrollPhysics(),
         child: Column(
           children: [
             const SizedBox(
@@ -47,64 +32,16 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(
               height: 10,
             ),
-            Column(
-              children: [
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                     productItem(),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      productItem(),
-                      
-                      /* Container(
-                        decoration: BoxDecoration(
-                          color: ColorsTheme.wight,
-                          borderRadius: BorderRadius.circular(8)
-                        ),
-                        alignment: Alignment.topCenter,
-                        width: 235,
-                        height: 400,
-                        padding: EdgeInsets.all(8),
-                        child: Image(
-                          image: NetworkImage(
-                            'https://images.unsplash.com/photo-1613915617430-8ab0fd7c6baf?ixlib=rb-1.2.1&raw_url=true&q=60&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500'
-                          ),
-                        ),
-                      ), */
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                      height: 10,
-                    ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      productItemTest(),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      productItemTest()
-                    ],
-                  ),
-                ),
-              ],
-            ),
-             const SizedBox(
-              height: 50,
-            ),
+            ProductPage(),
           ],
         ),
       ),
     );
   }
-  Widget productItem(){
+
+
+
+/*   Widget productItem(){
     return  Stack(
       children: [
         Container(
@@ -153,55 +90,5 @@ class _HomePageState extends State<HomePage> {
         ),
       ],
     );
-  }
-   Widget productItemTest(){
-    return  Stack(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            color: ColorsTheme.wight,
-            borderRadius: BorderRadius.circular(8)
-          ),
-          alignment: Alignment.topCenter,
-          width: 235,
-          height: 400,
-          padding: EdgeInsets.all(8),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 10,
-              ),
-              Image(
-                image: NetworkImage(
-                  'https://media.istockphoto.com/photos/image-of-brunette-in-fake-fur-coat-with-hood-picture-id535313567?k=20&m=535313567&s=612x612&w=0&h=xU51bRMrO0IV_uhBeys4orEUjktUbwzJVszpNgikKOY='
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text('Zara')
-            ],
-          ),
-        ),
-        Positioned(
-          right: 10,
-          // top: 6,
-          bottom: 45,
-          // right: 6.5,
-          child:
-            IconButton(
-            icon: Icon(
-              select2 ? iconselect2: iconUnSelect2
-            ),
-            onPressed: () {
-              setState(() {
-                select2 = !select2 ;
-              });
-            },
-          ),
-        ),
-      ],
-    );
-  }
+  } */
 }
