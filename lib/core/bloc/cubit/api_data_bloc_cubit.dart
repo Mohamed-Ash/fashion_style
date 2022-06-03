63f4aa58410d6b7ca1b949bc965137ee18b93fb6
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-03 09:19:14 -0700

    add home & basket page

--- lib/core/bloc/cubit/api_data_bloc_cubit.dart
+++ lib/core/bloc/cubit/api_data_bloc_cubit.dart
@@ -1,4 +1,5 @@
 
+import 'package:fashion_style/user/basket/basket_page/basket_page.dart';
 import 'package:fashion_style/user/home/home_page.dart';
 import 'package:fashion_style/user/offers/offers_page.dart';
 import 'package:fashion_style/user/sales/sales_page.dart';
@@ -10,16 +11,18 @@ part 'api_data_bloc_state.dart';
 class ApiDataBloc extends Cubit<ApiDataBlocState> {
   ApiDataBloc() : super(ApiDataBlocInitial());
   static ApiDataBloc get(context) => BlocProvider.of(context); 
-  int currentIndex = 0 ; 
-  //asdsdasdasd
+
+  int currentIndex = 0 ;
 
   List<Widget> screens = [
   const HomePage(),
-  const Offerspage(),
   const SalesPage(),
+  const Offerspage(),
+  const BasketPage()
   ];
  void changeIndex(int index){
    currentIndex = index ;
    emit(ChangeIndexState());
  }
+ 
 }
