4c32bd3204a42610dc23c2e1cce5e39d9d4cb4fb
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-03 09:20:33 -0700

    create app bar page & search

---
+++ lib/core/layout/app_bar_page/app_bar_page.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:fashion_style/core/router/string_route.dart';
+import 'package:fashion_style/core/theme/colors/colors_theme.dart';
+import 'package:flutter/material.dart';
+
+class AppBarPage extends StatelessWidget implements PreferredSizeWidget {
+  const AppBarPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      floating: true,
+      title: Text('fashion style'),
+      actions: [
+        IconButton(
+          onPressed: (){
+            Navigator.pushNamed(context, testpage);
+          }, 
+          icon: Icon(Icons.notifications_none),
+        ),
+      ],
+      bottom: PreferredSize(
+        child:Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: InkWell(
+            onTap: (){
+              Navigator.pushNamed(context, search);
+            },
+            child: Container(
+              width: double.infinity,
+              height: 40,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(12, 0, 20, 0),
+                    child: Icon(
+                      Icons.search,
+                      color: ColorsTheme.gray
+                    ),
+                  ),
+                  Text(
+                    'Do you need something !',
+                    style: TextStyle(
+                      color: ColorsTheme.gray
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        preferredSize: Size.fromHeight(50)
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(100);
+  // @override
+  // Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
---
+++ lib/user/search/search_page/search_page.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
