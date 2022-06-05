ac86ea6fb6585ef79d7d9d03eb20a25c1dbd0cf5
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-05 13:12:48 -0700

    Update page_layout_interface.dart

--- lib/core/layout/page_layout_interface.dart
+++ lib/core/layout/page_layout_interface.dart
@@ -1,12 +1,9 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, must_be_immutable
 
-import 'package:fashion_style/user/menu/drawer_page/drawer_page.dart';
 import 'package:flutter/material.dart';
 
 abstract class PageLayoutInterface extends StatelessWidget {
-  final GlobalKey<ScaffoldState> scaffoldKye = GlobalKey();
-  ScrollController? scrollController;
-  
+ bool? isAppBar ;
   PageLayoutInterface({Key? key,}) : super(key: key);
 
   @override
@@ -22,7 +19,7 @@ abstract class PageLayoutInterface extends StatelessWidget {
   // Widget? buildDrawer() => DrawerPage();
   Widget? get  buildDrawer => null ;
 
-  PreferredSizeWidget? get buildAppBar => AppBar();
+  PreferredSizeWidget? get buildAppBar => null;
 
   Widget buildBody(BuildContext context);
   
