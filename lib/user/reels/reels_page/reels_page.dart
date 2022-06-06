e1280a687abeff6eb0160241125dbd8ff0792275
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-06 10:32:11 -0700

    create reels video

--- lib/user/reels/reels_page/reels_page.dart
+++ lib/user/reels/reels_page/reels_page.dart
@@ -1,25 +1,62 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 // ignore: must_be_immutable
-class ReelsPage extends StatelessWidget {
+class ReelsPage extends StatefulWidget {
+  const ReelsPage({Key? key}) : super(key: key);
 
-  ReelsPage({Key? key}) : super(key: key);
+  @override
+  State<ReelsPage> createState() => _ReelsPageState();
+}
 
+class _ReelsPageState extends State<ReelsPage> {
   PageController controller = PageController(initialPage: 0);
+
+   late VideoPlayerController _videoPlayerController;
+  @override
+  void initState() {
+    super.initState();
+    _videoPlayerController = VideoPlayerController.asset('assets/videos/adidas.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+        _videoPlayerController.play();
+      });
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> reel = [
+    return SafeArea(
+      child: PageView(
+        scrollDirection: Axis.vertical,
+        controller: controller,
+        children:content(context),
+      ) ,
+    );
+  }
+
+  List<Widget> content(BuildContext context){
+    return [
       Container(
         width: MediaQuery.of(context).size.width,
         height:  MediaQuery.of(context).size.height,
-        color: Colors.blue,
+        color: Colors.grey,
       ),
       Container(
         width: MediaQuery.of(context).size.width,
         height:  MediaQuery.of(context).size.height,
-        color: Colors.green,
+        child: VideoPlayer(_videoPlayerController),
+      ),
+      Container(
+        width: MediaQuery.of(context).size.width,
+        height:  MediaQuery.of(context).size.height,
+        child: VideoPlayer(_videoPlayerController),
       ),
       Container(
         width: MediaQuery.of(context).size.width,
@@ -27,18 +64,6 @@ class ReelsPage extends StatelessWidget {
         color: Colors.grey,
       ),
     ];
- 
-    return SafeArea(
-      top: true,
-      bottom: true,
-      left: true,
-      right: true,
-      child: PageView(
-        scrollDirection: Axis.vertical,
-        children: reel,
-        controller: controller,
-      ) ,
-    );
   }
 }
 
