05e6450e7a3ebe270b011ea97f785b1567207af6
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-08 07:01:08 -0700

    add admin drawer and app bar

--- lib/admin/interface/admin_interface.dart
+++ lib/admin/interface/admin_interface.dart
@@ -1,13 +1,16 @@
-// ignore_for_file: non_constant_identifier_names, override_on_non_overriding_member, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, prefer_const_constructors
 
+import 'package:fashion_style/admin/admin_darwer/admin_darwer.dart';
 import 'package:fashion_style/core/layout/page_layout_interface.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
-class AdminInterface extends PageLayoutInterface {
+abstract class AdminInterface extends PageLayoutInterface {
   AdminInterface({Key? key}) : super(key: key);
 
-// mixin AdminInterface implements PageLayoutInterface {
+  @override
+  PreferredSizeWidget? get buildAppBar => AppBar();
+  @override
+  Widget? get buildDrawer => const AdminDrawer();
   
   @override
   Widget buildBody(BuildContext context) {
