5242aeb6304a69dc61b3c4db3e908cbe5a50ba3d
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-08 09:40:36 -0700

    Update forget_password.dart

--- lib/core/auth/forget_password/forget_password.dart
+++ lib/core/auth/forget_password/forget_password.dart
@@ -1,10 +1,12 @@
+import 'package:fashion_style/core/layout/page_layout_interface.dart';
 import 'package:flutter/material.dart';
 
-class ForgetPassword extends StatelessWidget {
-  const ForgetPassword({Key? key}) : super(key: key);
+// ignore: must_be_immutable
+class ForgetPassword extends PageLayoutInterface {
+  ForgetPassword({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget buildBody(BuildContext context) {
     return Container();
   }
 }
