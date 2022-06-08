50524471c03e3255e1a79a63430b84cbb49ef2f9
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-08 09:39:32 -0700

    Update string_route.dart

--- lib/core/router/string_route.dart
+++ lib/core/router/string_route.dart
@@ -1,17 +1,34 @@
+// Todo: auth 
 const String  login = '/' ;
 const String  register = '/register' ;
-const String  layout = '/layout' ;
-const String  admin = '/admin' ;
-const String  testpage = '/testpage' ;
 const String  craeteUserProfile = '/craeteuserprofile';
+const String  forgetPassword = '/forget_password';
+
+
+// Todo: user 
+const String  testpage = '/testpage' ;
 const String  userProfilePage = '/userprofilepage';
 const String  setting = '/setting';
-const String  accountsales = '/accountsales';
 const String  favorite = '/favorite';
 const String  product = '/product';
 const String  sales = '/sales';
 const String  producitem = '/producitem';
-const String  home = '/home';
+const String  appLayoutPage = '/app_Layout_Page';
 const String  search = '/search';
 const String  appPage = '/app_page';
+const String  activeSales = '/active_sales';
+
+
+
+// Todo: admin   
 const String  adminDashboard = '/admin_dashboard';
+const String  admin = '/admin' ;
+const String  adminAllOrders = '/admin_all_orders';
+const String  adminReels = '/admin_reels';
+const String  adminSalesNumber = '/admin_sales_nmber';
+const String  adminSalesProduct = '/admin_sales_product';
+const String  adminUserNumber = '/admin_user_nmber';
+
+
+// Todo: accountSales 
+const String  accountSales = '/accountsales';
