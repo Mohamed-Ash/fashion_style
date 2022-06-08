674fb6344f2eae13bb5673f301e2f8ea264b12c1
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-08 09:37:47 -0700

    create admin user number

---
+++ lib/admin/admin_user_number/admin_user_nmber_page/admin_user_number_page.dart
@@ -0,0 +1,16 @@
+import 'package:fashion_style/admin/admin_user_number/admin_user_number_widget/admin_user_number_widget.dart';
+import 'package:fashion_style/admin/interface/admin_interface.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class AdminUserNumberPage extends AdminInterface {
+  AdminUserNumberPage({Key? key}) : super(key: key);
+
+  @override
+  String get title => 'User Number';
+
+  @override
+  Widget buildBody(BuildContext context) {
+    return const AdminUserNumberWidget();
+  }
+}
---
+++ lib/admin/admin_user_number/admin_user_number_widget/admin_user_number_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdminUserNumberWidget extends StatelessWidget {
+  const AdminUserNumberWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
