ba2198decea0ece9567760d3585376d951fbc200
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-08 09:34:37 -0700

    create admin reels

---
+++ lib/admin/admin_reels/admin_reels_page/admin_reels.dart
@@ -0,0 +1,16 @@
+import 'package:fashion_style/admin/admin_reels/admin_reels_widget/admin_reels_widget.dart';
+import 'package:fashion_style/admin/interface/admin_interface.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class AdminReels extends AdminInterface{
+  AdminReels({Key? key}) : super(key: key);
+
+  @override
+  String get title => 'Admin Reels';
+
+  @override
+  Widget buildBody(BuildContext context) {
+    return const AdminReelsWidget();
+  }
+}
---
+++ lib/admin/admin_reels/admin_reels_widget/admin_reels_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdminReelsWidget extends StatelessWidget {
+  const AdminReelsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
