64c471b180579d7af6c4afda2bce2c7a6b93044c
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-08 09:35:10 -0700

    create admin sales number

---
+++ lib/admin/admin_sales_number/admin_sales_number_page/admin_sales_number_page.dart
@@ -0,0 +1,16 @@
+import 'package:fashion_style/admin/admin_sales_number/admin_sales_number_widget/admin_sales_number_widget.dart';
+import 'package:fashion_style/admin/interface/admin_interface.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class AdminSalesNumberPage extends AdminInterface{
+  AdminSalesNumberPage({Key? key}) : super(key: key);
+
+  @override
+  Widget buildBody(BuildContext context) {
+    return const AdminSalesNumberWidget();
+  }
+
+  @override
+  String get title => 'Sales Nmber';
+}
