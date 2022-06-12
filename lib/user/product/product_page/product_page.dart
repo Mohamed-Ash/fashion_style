1f76906024e6c20bf65fc5054dd339ec1d1852f3
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-12 13:49:36 -0700

    update name user profile & product

--- lib/user/product/product_page/product_page.dart
+++ lib/user/product/product_page/product_page.dart
@@ -1,7 +1,7 @@
 
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:fashion_style/user/product_item/product_widget/product_widget.dart';
+import 'package:fashion_style/user/product/product_widget/product_widget.dart';
 import 'package:flutter/material.dart';
 
 
