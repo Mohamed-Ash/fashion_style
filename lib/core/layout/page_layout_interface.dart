d89e40f021de88316c4649716d7ac0c0593685ce
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-12 15:48:34 -0700

    Update page_layout_interface.dart

--- lib/core/layout/page_layout_interface.dart
+++ lib/core/layout/page_layout_interface.dart
@@ -3,8 +3,7 @@
 import 'package:flutter/material.dart';
 
 abstract class PageLayoutInterface extends StatelessWidget {
- bool? isAppBar ;
-  PageLayoutInterface({Key? key,}) : super(key: key);
+  const PageLayoutInterface({Key? key,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +11,9 @@ abstract class PageLayoutInterface extends StatelessWidget {
       // todo : build new app bar without sliver
       appBar: buildAppBar,
       drawer: buildDrawer,
-      body: buildBody(context),
+      body: SafeArea(
+        child: buildBody(context)
+      ),
     );
   }
 
