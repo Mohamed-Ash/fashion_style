20b80bf007d9cda506427806c4182b2d4d9f5d81
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-13 09:43:26 -0700

    delete folder bloc

--- lib/account_sales/cubit/bloc_sales_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'bloc_sales_state.dart';
-
-class BlocSalesCubit extends Cubit<BlocSalesState> {
-  BlocSalesCubit() : super(BlocSalesInitial());
-}
--- lib/account_sales/cubit/bloc_sales_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'bloc_sales_cubit.dart';
-
-@immutable
-abstract class BlocSalesState {}
-
-class BlocSalesInitial extends BlocSalesState {}
