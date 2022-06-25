c96d76e7db36e4e3fa961e16e80e2871579c0257
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-25 17:09:24 +0200

    Update favorite_page.dart

--- lib/user/favorite/favorite_page/favorite_page.dart
+++ lib/user/favorite/favorite_page/favorite_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_print
+// ignore_for_file: avoid_print, prefer_const_constructors, curly_braces_in_flow_control_structures
 
 import 'package:fashion_style/core/sql/sql.dart';
 import 'package:fashion_style/user/user_interface.dart';
@@ -8,14 +8,71 @@ import 'package:flutter/material.dart';
 class FavoritePage extends UserInterface {
   CreatSQl createSQl = CreatSQl();
   
+  Future<List<Map>> readData()async{
+    List<Map> response = await  createSQl.readData("SELECT * FROM 'product' ");
+    return response;
+  }
   FavoritePage({Key? key}) : super(key: key);
   
   @override
   Widget buildBody(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          MaterialButton(
+    return ListView(
+
+      children: [
+        MaterialButton(
+          child: Text('delete database'),
+          onPressed: ()async{
+            await createSQl.myDeleteData();
+          }
+        ),
+        FutureBuilder(
+          future: readData(),
+          builder: (BuildContext context, AsyncSnapshot<List<Map>> snapshot){
+            if(snapshot.hasData){
+              return ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context,index){
+                  return ListTile(
+                    leading: Text("${snapshot.data![index]['name']}"),
+                    title:  Text("${snapshot.data![index]['title']}"),
+                    subtitle: Text("${snapshot.data![index]['price']}"),
+                    );
+                }
+              );
+            }
+             return CircularProgressIndicator();
+          }
+        ),
+      ],
+    );
+  }
+
+  @override
+  String get title => 'favorite page' ;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  MaterialButton(
             onPressed:()async{
               int response = await createSQl.insertData(
                 "INSERT INTO 'product' ('name','title','price') VALUES ('note','tow', 'three')"
@@ -39,18 +96,8 @@ class FavoritePage extends UserInterface {
           ),
           MaterialButton(
             onPressed:()async{
-              List<Map> response = await createSQl.readData("DELETE FROM 'product' WHERE id = 10 ");
+              List<Map> response = await createSQl.readData("DELETE FROM 'product' WHERE id = 23 ");
               print("$response");
             } ,
             child: const Text('insert Data'), 
-          ),
-        ],
-   ),
-    );
-  }
-
-  @override
-  String get title => 'favorite page' ;
-
-
-}
+          ), */
