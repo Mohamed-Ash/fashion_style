c5d3d5273088a3255dcd786111b4433d3b3172ac
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-06-28 17:43:56 +0200

    fix to import

--- lib/user/payments/page/payments_page.dart
+++ lib/user/payments/page/payments_page.dart
@@ -1,7 +1,6 @@
-import 'package:fashion_style/core/bloc/user_cubit/user_state.dart';
 import 'package:fashion_style/user/payments/widget/payments_widget.dart';
 import 'package:fashion_style/user/user_interface.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class PaymentsPage extends UserInterface {
   const PaymentsPage({Key? key}) : super(key: key);
