9d12da000ac0d6c6baabf6771de4d9b7314a7dc1
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2022-07-08 18:02:18 +0200

    transfer basket page from drawer to buttom navigation bar
    transfer basket page from drawer to buttom navigation bar

--- lib/user/basket/basket_page/basket_page.dart
+++ lib/user/basket/basket_page/basket_page.dart
@@ -1,10 +1,15 @@
+import 'package:fashion_style/user/user_interface.dart';
 import 'package:flutter/material.dart';
 
-class BasketPage extends StatelessWidget {
+class BasketPage extends UserInterface {
   const BasketPage({Key? key}) : super(key: key);
-
+  
   @override
-  Widget build(BuildContext context) {
+  Widget buildBody(BuildContext context) {
     return Container();
   }
+  
+  @override
+  String? get title => '';
+
 }
